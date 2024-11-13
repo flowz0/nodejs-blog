e04f6b2ed13a5cd2335a3ae33b29bb5d5d154700
flowz0 <billylflowers@gmail.com>
2024-11-13 02:10:08 -0800

    Created a slug route

--- server/routes/main.js
+++ server/routes/main.js
@@ -36,6 +36,28 @@ router.get('', async (req, res) => {
 
 });
 
+/* 
+    GET / Post :id
+ */
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug });
+
+        const locals = {
+            title: data.title,
+            description: "Simple blog created with NodeJS, Express, and MongoDB."
+        }
+
+        res.render('post', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
 router.get('/about', (req, res) => {
     res.render('about');
 });
